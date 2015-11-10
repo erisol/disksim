ad682e471ac785d8aa62a48394700a69bfe13200
Freysev <freysev@hotmail.com>
2015-11-10 16:35:37 +0100

    removed solbakk_pdf.m

--- solbakk_pdf.m
+++
@@ -1,6 +0,0 @@
-function [pns] = solbakk_pdf()
-pns.PN_name = 'Surplus mass handling - Solbakk';
-pns.set_of_Ps = {'pSolbakk', 'pBand', 'pSeadump'};
-pns.set_of_Ts = {'tLoad','tTransport','tKnus'};
-pns.set_of_As =  {'pSolbakk', 'tLoad', 1, 'tLoad', 'pBånd', 1, 'pBand', 'pSjoefylling', 1};   
-    
