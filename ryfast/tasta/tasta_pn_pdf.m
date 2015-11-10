6237be03a4f3caf7a5cf83182521f12c03d3767f
Freysev <freysev@hotmail.com>
2015-11-10 17:23:12 +0100

    added place buffer between roadTo and wash

--- ryfast/tasta/tasta_pn_pdf.m
+++ ryfast/tasta/tasta_pn_pdf.m
@@ -1,5 +1,5 @@
 function [pns] = tasta_pn_pdf()
 pns.PN_name = 'Surplus mass handling - Tasta';
-pns.set_of_Ps = {'pTasta','pRoadFrom','pJaattaa','pRoadTo'};
+pns.set_of_Ps = {'pTasta','pRoadFrom','pJaattaa','pRoadTo','pBuffer'};
 pns.set_of_Ts = {'tLoad','tTransFrom','tDump','tTransTo', 'tWash'};
-pns.set_of_As =  {'pTasta','tLoad', 1, 'tLoad','pRoadFrom', 1, 'pRoadFrom','tTransFrom', 1, 'tTransFrom','pJaattaa', 1, 'pJaattaa','tDump', 1, 'tDump','pRoadTo', 1, 'pRoadTo','tTransTo', 1, 'tTransTo','tWash', 1, 'tWash','pTasta', 1};
+pns.set_of_As =  {'pTasta','tLoad', 1, 'tLoad','pRoadFrom', 1, 'pRoadFrom','tTransFrom', 1, 'tTransFrom','pJaattaa', 1, 'pJaattaa','tDump', 1, 'tDump','pRoadTo', 1, 'pRoadTo','tTransTo', 1, 'tTransTo','pBuffer', 1, 'pBuffer', 'tWash', 1,'tWash','pTasta', 1};
