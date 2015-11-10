380fb0b0a28feff79db710fbf235784b5ed1178c
Freysev <freysev@hotmail.com>
2015-11-10 16:34:44 +0100

    first runnable petrinet

--- solbakk_til_sjoefylling.m
+++
@@ -1,5 +0,0 @@
-function [pns] = solbakk_pdf()
-pns.PN_name = 'Surplus mass handling - Solbakk';
-pns.set_of_Ps = {'pSolbakk', 'pBand', 'pSeadump'};
-pns.set_of_Ts = {'tLoad','tTransport','tCrush'};
-pns.set_of_As =  {'pSolbakk', 'tLoad', 1, 'tLoad', 'pBand', 1, 'pBand', 'tCrush', 1, 'tCrush', 'pSeadump', 1};   
