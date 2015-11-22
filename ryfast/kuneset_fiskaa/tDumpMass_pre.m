62a63d19edea03c9be0419bbd902f80f930b1849
Freysev <freysev@hotmail.com>
2015-11-22 16:14:18 +0100

    fiskå done

--- ryfast/kuneset_fiskaa/tDumpMass_pre.m
+++ ryfast/kuneset_fiskaa/tDumpMass_pre.m
@@ -7,8 +7,8 @@ colors = get_color('pEnroute',tokID);
 
 if current_time >= str2double(colors{1})
     fprintf('Time %s: \t Place: pDumpAtFiskaa \t\t Action: Load dumped at Fiskaa\n',string_HH_MM_SS(current_time()));
-    global_info.dumped_at_fiskaa = global_info.dumped_at_fiskaa + global_info.truck_capacity;
-    transition.new_color = num2str(str2double(colors{1})+60*randi([10,13],1,1));
+    global_info.dumped_at_fiskaa = global_info.dumped_at_fiskaa + global_info.barge_capacity;
+    transition.new_color = num2str(str2double(colors{1})+60*60*3);
     transition.override = 1;
     fire = tokID;
 end
