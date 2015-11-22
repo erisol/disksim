ad18dca235c148398983d5f78346abf1c1694781
Freysev <freysev@hotmail.com>
2015-11-22 16:21:45 +0100

    rosenberg done

--- ryfast/kuneset_rosenberg/tDumpMass_pre.m
+++ ryfast/kuneset_rosenberg/tDumpMass_pre.m
@@ -7,8 +7,8 @@ colors = get_color('pEnroute',tokID);
 
 if current_time >= str2double(colors{1})
     fprintf('Time %s: \t Place: pDumpAtRosenberg \t Action: Load dumped at Rosenberg\n',string_HH_MM_SS(current_time()));
-    global_info.dumped_at_rosenberg = global_info.dumped_at_rosenberg + global_info.truck_capacity;
-    transition.new_color = num2str(str2double(colors{1})+60*randi([10,13],1,1));
+    global_info.dumped_at_rosenberg = global_info.dumped_at_rosenberg + global_info.barge_capacity;
+    transition.new_color = num2str(str2double(colors{1})+60*randi([20,30],1,1));
     transition.override = 1;
     fire = tokID;
 end
